05d4b6cf2b4c075c5a974e9ce56f0be2797561bc
samuelpinerosr <spinerosr@unicartagena.edu.co>
2026-06-09 09:37:21 -0500

    Agregar conversión de días a horas

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ingrese la cantidad de días: ");
+        int dias = input.nextInt();
+
+        int horas = dias * 24;
+
+        System.out.println(dias + " días equivalen a " + horas + " horas.");
+
+        input.close();
     }
 }
